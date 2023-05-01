30ba473f370daeeda71d078fc94f2121ff0c1cfa
Sahil020701 <89337983+Sahil020701@users.noreply.github.com>
2023-05-01 16:26:18 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ function Hero() {
         {/* left side  */}
         <div className="left">
           <p>Buy & Sell Crypto 24/7 using your retirement account</p>
-          <h1>Invest in Cryptocurrency with Your IRA</h1>
+          <h1>Invest in Cryptocurrency with Your investment strategist Sahil Gobade</h1>
           <p>Buy, Sell and store hundreds of cryptocurrencies</p>
           <div className="input-container">
             <input type="email" placeholder="Enter your email"></input>
